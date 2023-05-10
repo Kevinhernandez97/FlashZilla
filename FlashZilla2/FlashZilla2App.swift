ea811664f4e9b578b948ab92881dbfec7295ebf8
Kevin Hernandez <kmhs-18@hotmail.com>
2023-05-10 11:38:18 -0300

    Initial Commit

---
+++ FlashZilla2/FlashZilla2App.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZilla2App.swift
+//  FlashZilla2
+//
+//  Created by Kevin Hernandez on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashZilla2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
