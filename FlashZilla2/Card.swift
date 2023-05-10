88035ca3898b47c848dddaace21c59aa2a8d8c64
Kevin Hernandez <kmhs-18@hotmail.com>
2023-05-10 17:09:16 -0300

    new project adassets, and cards swiftUI with 2 views

---
+++ FlashZilla2/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  FlashZilla2
+//
+//  Created by Kevin Hernandez on 10/05/23.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who plated tge 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
